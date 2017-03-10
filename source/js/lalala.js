7ec68d23304939fa2500af7b8ef05cb11afaf423
firefoxic <firefoxic.arts@gmail.com>
2017-03-10 20:20:52 +0300

    Добавь базовые исходники для тестов настроек

---
+++ source/js/lalala.js
@@ -0,0 +1,9 @@
+'use strict';
+
+window.lalala = (function () {
+  return {
+    lalala: function () {
+      return 'lalala';
+    }
+  };
+})();
---
+++ source/js/tututu.js
@@ -0,0 +1,9 @@
+'use strict';
+
+window.tututu = (function () {
+  return {
+    tututu: function () {
+      return 'tututu';
+    }
+  };
+})();
