49ce6002ef9192488bd92de855bf209900fac4fe
firefoxic <firefoxic.arts@gmail.com>
2017-03-11 09:17:30 +0300

    Добавь генерацию sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const postcss = require('gulp-postcss');
 const rename = require('gulp-rename');
 const autoprefixer = require('autoprefixer');
 const atImport = require("postcss-import");
+const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('styles', function() {
   var plugins = [
@@ -12,7 +13,9 @@ gulp.task('styles', function() {
     autoprefixer()
   ];
   return gulp.src('source/css/style.post.css')
+    .pipe(sourcemaps.init())
     .pipe(postcss(plugins))
     .pipe(rename('style.css'))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('build'));
 });
