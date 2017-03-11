400afcced1516200f0f2a6cd571c797da154b63d
firefoxic <firefoxic.arts@gmail.com>
2017-03-11 11:08:32 +0300

    Генерируй sourcemap только для разработки
    С помощью плагина gulp-if проверяем, обработка происходит для продакшена
    или для девелопмента. Если второе — генерируем sourcemap, иначе делаем
    сборку без sourcemap.

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ const rename = require('gulp-rename');
 const autoprefixer = require('autoprefixer');
 const atImport = require("postcss-import");
 const sourcemaps = require('gulp-sourcemaps');
+const gulpIf = require('gulp-if');
+
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
 gulp.task('styles', function() {
   var plugins = [
@@ -13,9 +16,9 @@ gulp.task('styles', function() {
     autoprefixer()
   ];
   return gulp.src('source/css/style.post.css')
-    .pipe(sourcemaps.init())
+    .pipe(gulpIf(isDevelopment, sourcemaps.init()))
     .pipe(postcss(plugins))
     .pipe(rename('style.css'))
-    .pipe(sourcemaps.write())
+    .pipe(gulpIf(isDevelopment, sourcemaps.write()))
     .pipe(gulp.dest('build'));
 });
