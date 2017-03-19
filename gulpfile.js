9ffff631ce4b4f907ce29041ea7ab3866ea5f7db
firefoxic <firefoxic.arts@gmail.com>
2017-03-19 21:02:08 +0300

    Убери путь из параметров функции запуска задач
    Не зачем постоянно, при каждом вызове функции указывать путь, учитывая,
    что паттерн пути всегда постоянный: './task/' с припиской имени задачи.
    Эту приписку можно сделать внутри самой функции.

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,16 @@
 
 const gulp = require('gulp');
 
-function runTaskLazy(taskName, pathToTask) {
-  function requireTask(callback) {
-    return require(pathToTask)()(callback);
-  }
-  gulp.task(taskName, requireTask);
+function runTaskLazy(taskName) {
+  gulp.task(taskName, (callback) => require('./tasks/' + taskName)()(callback));
 }
 
-runTaskLazy('pages', './tasks/pages');
-runTaskLazy('styles', './tasks/styles');
-runTaskLazy('scripts', './tasks/scripts');
-runTaskLazy('assets', './tasks/assets');
-runTaskLazy('clean', './tasks/clean');
-runTaskLazy('build', './tasks/build');
-runTaskLazy('watch', './tasks/watch');
-runTaskLazy('serve', './tasks/serve');
-runTaskLazy('dev', './tasks/dev');
+runTaskLazy('pages');
+runTaskLazy('styles');
+runTaskLazy('scripts');
+runTaskLazy('assets');
+runTaskLazy('clean');
+runTaskLazy('build');
+runTaskLazy('watch');
+runTaskLazy('serve');
+runTaskLazy('dev');
