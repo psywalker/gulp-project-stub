a709b41db370ba01dc1cf0b73973dc4185ca3cce
firefoxic <firefoxic.arts@gmail.com>
2017-03-20 21:36:16 +0300

    Update 2017-03-20T18:36:04.173Z

---
+++ js/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+window.lalala = (function () {
+  return {
+    lalala() {
+      return 'lalala';
+    }
+  };
+})();
+
+'use strict';
+
+window.tututu = (function () {
+  return {
+    tututu() {
+      return 'tututu';
+    }
+  };
+})();
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImxhbGFsYS5qcyIsInR1dHV0dS5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQ1RBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyIndXNlIHN0cmljdCc7XG5cbndpbmRvdy5sYWxhbGEgPSAoZnVuY3Rpb24gKCkge1xuICByZXR1cm4ge1xuICAgIGxhbGFsYSgpIHtcbiAgICAgIHJldHVybiAnbGFsYWxhJztcbiAgICB9XG4gIH07XG59KSgpO1xuIiwiJ3VzZSBzdHJpY3QnO1xuXG53aW5kb3cudHV0dXR1ID0gKGZ1bmN0aW9uICgpIHtcbiAgcmV0dXJuIHtcbiAgICB0dXR1dHUoKSB7XG4gICAgICByZXR1cm4gJ3R1dHV0dSc7XG4gICAgfVxuICB9O1xufSkoKTtcbiJdfQ==
