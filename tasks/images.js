2fb718816b1205852e3e3f6e83e3af816269f85b
firefoxic <firefoxic.arts@gmail.com>
2017-03-31 18:07:44 +0300

    Замени плагин для обработки графики
    Плагин gulp-imagemin не справляется с поставленными задачами (нормально
    сжимает только svg, и то это не его заслуга, а svgo).
    В отличии от него gulp-image «из коробки» имеет хорошую настройку,
    не нуждающуюся в опциях (хотя, можно и вырубить какую-нибудь из
    обработок) и очень качественно оптимизирует графические файлы.

--- tasks/images.js
+++ tasks/images.js
@@ -3,28 +3,13 @@
 const gulp = require('gulp');
 const cached = require('gulp-cached');
 const remember = require('gulp-remember');
-const imagemin = require('gulp-imagemin');
+const image = require('gulp-image');
 
 module.exports = function () {
   return function () {
     return gulp.src('source/raw/img/**/*.{png,jpg,gif,svg}')
       .pipe(cached('images'))
-      .pipe(imagemin([
-        imagemin.optipng({optimizationLevel: 3}),
-        imagemin.jpegtran({progressive: true}),
-        imagemin.gifsicle({interlaced: true}),
-        imagemin.svgo({
-          // js2svg: {
-          //   pretty: true
-          // },
-          plugins: [{
-            removeViewBox: true
-          }]
-        })
-      ], {
-        verbose: true
-      }
-      ))
+      .pipe(image())
       .pipe(remember('images'))
       .pipe(gulp.dest('source/assets/img'));
   };
