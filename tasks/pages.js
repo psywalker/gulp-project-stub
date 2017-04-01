47557bd414d93b8318fa1295a3464a6fd37f3c0a
firefoxic <firefoxic.arts@gmail.com>
2017-04-01 15:04:33 +0300

    Добавь инкрементальности в pages.js

--- tasks/pages.js
+++ tasks/pages.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const gulp = require('gulp');
+const newer = require('gulp-newer');
 
 module.exports = function () {
   return function () {
-    return gulp.src('source/*.html')
+    return gulp.src('source/*.html', {since: gulp.lastRun('pages')})
+      .pipe(newer('build'))
       .pipe(gulp.dest('build'));
   };
 };
