284ab813c3a38d0307ec63378b59e45d5fa51e9d
firefoxic <firefoxic.arts@gmail.com>
2017-04-01 15:48:05 +0300

    Дополни watch слежением за сырьём

--- tasks/watch.js
+++ tasks/watch.js
@@ -5,15 +5,19 @@ const gulp = require('gulp');
 const cached = require('gulp-cached');
 const remember = require('gulp-remember');
 
-function forgetCach(filepath) {
-  remember.forget('js', path.resolve(filepath));
-  delete cached.caches.js[path.resolve(filepath)];
-}
-
 module.exports = function () {
   return function () {
     gulp.watch('source/*.html', gulp.series('pages'));
-    gulp.watch('source/js/**/*.*', gulp.series('scripts')).on('unlink', forgetCach);
+    gulp.watch('source/raw/fonts/*.*', gulp.series('fonts'));
+    gulp.watch('source/raw/img/*.*', gulp.series('images'));
+    gulp.watch('source/raw/icons/*.*', gulp.series('icons')).on('unlink', function (filepath) {
+      remember.forget('icons', path.resolve(filepath));
+      delete cached.caches.icons[path.resolve(filepath)];
+    });
+    gulp.watch('source/js/**/*.*', gulp.series('scripts')).on('unlink', function (filepath) {
+      remember.forget('js', path.resolve(filepath));
+      delete cached.caches.js[path.resolve(filepath)];
+    });
     gulp.watch('source/css/**/*.*', gulp.series('styles'));
     gulp.watch('source/assets/**/*.*', gulp.series('copy'));
   };
