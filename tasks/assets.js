495effd51bb1a846c777a40dc5b1a48728d15944
firefoxic <firefoxic.arts@gmail.com>
2017-04-01 16:45:09 +0300

    fixup (удали очистку assets из assets.js)

--- tasks/assets.js
+++ tasks/assets.js
@@ -1,8 +1,7 @@
 'use strict';
 
-const del = require('del');
 const gulp = require('gulp');
 
 module.exports = function () {
-  return gulp.series(() => del(['source/assets']), gulp.parallel('fonts', 'icons', 'images'));
+  return gulp.parallel('fonts', 'icons', 'images');
 };
